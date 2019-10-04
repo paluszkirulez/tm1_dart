a4be9aa1c9f42391b0bc1a542164e010655d6367
Dawid <dawidb@emea.roche.com>
2019-10-04 12:47:07 +0200

    test or dimension added - possibility to create dimension

--- test/Object/dimension_test.dart
+++ test/Object/dimension_test.dart
@@ -1,8 +1,9 @@
 import 'package:tm1_dart/Objects/Dimension.dart';
 import 'package:tm1_dart/Services/DimensionService.dart';
+import 'package:tm1_dart/Services/HierarchyService.dart';
 import 'package:tm1_dart/Services/RESTConnection.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:random_string/random_string.dart';
 import '../UtilsForTest/ConnectionUtils.dart';
 
 void main() async {
@@ -27,10 +28,29 @@ void main() async {
     expect(printout.name, expectedDimension.name);
   });
   test('check if correct hierarchies are returned', () async {
-    List<String> expectedElements = ['actvsbud','actvsbud2','Leaves','O#3)3', '=Z%G1'];
+    List<String> expectedElements = [
+      'actvsbud',
+      'actvsbud2',
+      'Leaves',
+      'O#3)3',
+      '=Z%G1',
+      '_a.Kj'
+    ];
     expectedElements.sort((a, b) => a.hashCode.compareTo(b.hashCode));
     var printout = await DimensionService().getHierarchies(dimension);
     printout.sort((a, b) => a.hashCode.compareTo(b.hashCode));
     expect(printout, expectedElements);
   });
+  test('check if dimension is created', () async {
+    String randomDim = randomAlpha(5);
+    print(randomDim);
+    Dimension randomDimension = Dimension.fromJson({'Name': randomDim});
+    bool result = await DimensionService().create(randomDimension);
+    bool result2 = await DimensionService().checkIfExists(randomDimension);
+    expect(result, true);
+    expect(result2, true);
+  });
+
+
+
 }
